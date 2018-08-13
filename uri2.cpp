196343dcfda14c1bf80bfb00d757cfdab4517be4
Joao Pedro Ramos Moisakis <jprm@ug3c39.windows.cin.ufpe.br>
2018-08-13 20:44:16 -0300

    add uri1 and uri2

---
+++ uri2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath> 
+#include <iomanip>
+
+using namespace std;
+
+
+int main() {
+ 
+    double raio;
+    double pi = 3.14159;
+    cin >> raio;
+    double area = pi * raio*raio;
+    cout << "A=" << area << std::fixed << std::setprecision(4);
+    //double d = 122.345;
+
+    //std::cout << std::fixed <<<std::setprecision(2);
+    //std::cout << ;
+    //::cout << d;
+    return 0;
+}
