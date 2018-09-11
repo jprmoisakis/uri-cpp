438cd20f299815b3588d8ebe9532f7e1f02cb4be
João Pedro <jprmoisakis@gmail.com>
2018-09-10 23:05:59 -0300

    working but not in the right way

--- CodeForces1015B.cpp
+++ CodeForces1015B.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
-
+#include <algorithm>
+#include <string>
 using namespace std;
+
+bool AreAnagrams(string s, string t) {
+    sort(s.begin(), s.end());
+    sort(t.begin(), t.end());
+    return s == t;
+}
 
 int main(){
 	int n;
@@ -11,26 +18,47 @@ int main(){
 	cin >> s;
 	cin >> t;
 
-	int low = 0, high = n-1,aux;
+	int low = 0, high = n-1,aux,swapPos[10000],nSwaps=0;
 
-	while(low <high){
-
-		if(s[low] != t[low]){
-			aux = s[low];
-			s[low] = s[low +1];
-			s[low + 1] = aux;
+	if(!AreAnagrams(s,t)){
+		cout <<"-1"<<endl;
+	}else if(s==t){
+		cout <<"0"<<endl;
+	}else{
+		while(s !=t){
+			while(low < n -1 && high > 0){
+				if(s[low] != t[low]){
+					aux = s[low];
+					s[low] = s[low +1];
+					s[low + 1] = aux;
+					cout << "low = "<<s<<endl;
+					swapPos[nSwaps] = low;
+					nSwaps++;
+				}
+				low++	;	
+			}
+			while(high > 0){
+				if(s[high] != t[high]){
+					aux = s[high];
+					s[high] = s[high -1];
+					s[high - 1] = aux;
+					cout <<"high = "<< s<<endl;
+					swapPos[nSwaps] = high;
+					nSwaps++;
+				}
+				high--;					
+				
+			}
+			high = n-1;
+			low = 0;
 		}
-		if(s[high] != t[high]){
-			aux = s[high];
-			s[high] = s[high -1];
-			s[high - 1] = aux;
+		cout << s <<endl;
+		cout << t <<endl;
+		cout << nSwaps <<endl;
+		for (int i = 0; i < nSwaps; i++){
+			cout <<swapPos[i] <<" ";
 		}
-
 	}
-
-	cout << s <<endl;
-	cout << t <<endl;
-
 
 	return 0;
 }
