47d3c6372ac56a4801fdf75d81f5712b51dc6b30
João Pedro Moisakis <joao.moisakis@localhost.localdomain>
2018-09-17 18:47:23 -0300

    incomplete

---
+++ homework#1/d.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <pair>
+using namespace std;
+
+
+int main(){
+    int n,d, aux1,aux2;
+    vector <pair<int,int>> vec;
+    cin >> n >>d;
+    pair <int int> p;
+
+    for(int i =0; i< n; i++){
+        cin >>aux1 >>aux;
+        p.first=aux1;
+        p.second = aux2;
+
+        vec.push_back(p);
+    }
+
+    
+
+
+    return 0;
+}
